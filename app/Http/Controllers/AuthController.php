9c6f4e5a76391c144fec22dfcb85fb90a7954b31
Virz <virgiriyadi2404@gmail.com>
2025-06-28 13:44:21 +0700

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\AuthRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(AuthRequest $request) {
+      $validated = $request->validated();
+
+      $registeredUser = User::firstWhere('email', $validated['email']);
+
+      if ($registeredUser) {
+        return response()->json([
+          'message' => 'Email sudah terdaftar'
+        ], 400);
+      }
+
+      $user = User::create([
+        'name' => $validated['name'],
+        'email' => $validated['email'],
+        'password' => Hash::make($validated['password'])
+      ]);
+
+      $token = $user->createToken('auth_token')->plainTextToken;
+
+      return response()->json([
+        'data' => $user,
+        'access_token' => $token,
+        'token_type' => 'Bearer'
+      ], 200);
+    }
+
+    public function login(Request $request) {
+      $credentials = $request->validate([
+        'email' => '|required|string|email',
+        'password' => 'required|string'
+      ]);
+
+      if (!Auth::attempt($credentials)) {
+        return response()->json([
+          'message' => 'Email atau password salah'
+        ], 401);
+      }
+
+      $user = User::where('email', $request->email)->firstOrFail();
+
+      $token = $user->createToken('auth_token')->plainTextToken;
+
+      return response()->json([
+        'message' => 'Login berhasil',
+        'access_token' => $token,
+        'token_type' => 'Bearer'
+      ]);
+    }
+
+    public function logout(Request $request) {
+      $request->user()->currentAccessToken()->delete();
+
+      return response()->json([
+        'message' => 'Logout berhasil'
+      ]);
+    }
+}
